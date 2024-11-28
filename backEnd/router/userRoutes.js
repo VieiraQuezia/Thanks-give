0458fcf26397f3d2db2ae48247c4c80f77775e64
Quezia Vieira <vieiraquezia24@gmail.com>
2024-11-28 17:25:43 -0300

    alt backend

--- backEnd/router/userRoutes.js
+++ backEnd/router/userRoutes.js
@@ -2,16 +2,13 @@
 
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
-
-// Rota GET para listar todos os usuários
-router.get("/mensagem", userController.getMensagem);
+const userController = require("../controller/userController");
 
 // Rota POST para criar um novo usuário
 router.post("/mensagem", userController.createMensagem);
 
 // Rota GET para buscar um usuário pelo ID
-router.get("/mensagem/:tema", userController.getMensagemByTema);
+router.get("/mensagem", userController.getMensagemAleatoria);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////HISTÓRIAS MOTIVADORAS////////////////////////////////////////////////////////////////////
